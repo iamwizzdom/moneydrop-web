79e10b11a3461dfaf5adafb1304baec1ba69e6bd
iamwizzdom <wisdomemenike70@gmail.com>
2021-05-11 20:18:56 +0100

    Built project

--- src/constants/app.js
+++ src/constants/app.js
@@ -5,7 +5,7 @@ export const AppConst = {
     MALE: 1,
     FEMALE: 2,
     PAGINATION_PER_PAGE: 30,
-    MONO_KEY: 'test_pk_QkHIcvviN20Tbyap1wnF',
+    MONO_KEY: 'live_sk_VEC06iUg48u91fXALulV',
     FLUTTERWAVE_PUBKEY: 'FLWPUBK-a46dd307fadd0d345c9ebba50b56c724-X',
     MIN_TOP_UP_AMOUNT: 1000,
 };
